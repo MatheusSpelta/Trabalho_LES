1ba1bd6a1285b84c0799e2766ec7fccc970e4894
Matheus Spelta <83254846+MatheusSpelta@users.noreply.github.com>
2025-04-24 17:31:39 -0300

    Atualiza a porta do servidor para 5020, altera o nome do banco de dados para letras minúsculas e refatora o código do servidor TCP para melhorar a legibilidade.

--- src/main/java/com/example/demo/config/TcpServerInitializer.java
+++ src/main/java/com/example/demo/config/TcpServerInitializer.java
@@ -1,13 +1,13 @@
-package com.example.demo.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.example.demo.tcp.TcpServer;
-
-import jakarta.annotation.PostConstruct;
-
-@Component
-public class TcpServerInitializer {
-
-}
+//package com.example.demo.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import com.example.demo.tcp.TcpServer;
+//
+//import jakarta.annotation.PostConstruct;
+//
+//@Component
+//public class TcpServerInitializer {
+//
+//
